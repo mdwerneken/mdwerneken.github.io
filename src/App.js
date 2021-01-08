1678608f405960d63ad6523d1d77c63940866261
Matthew <mdw2166@columbia.edu>
2021-01-07 23:17:29 -0500

    homepage now redirects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './images/dantheman.jpg';
+import logo from './images/gangsta.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p></p>
         <img src={logo} className="App-logo" alt="logo" />
-
+        <p></p>
+        
+        <h1>Whoopsie!</h1>
       </header>
+      <p>You must be looking for <a color="NavyBlue" href="mdwerneken.github.io/jade-practice-proj/">My JADE Practice Project!</a></p>
     </div>
   );
 }
